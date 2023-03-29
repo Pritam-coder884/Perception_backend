30fa6ef4268c1855bf6c7e36c71a070236840f19
Pritam kumar dash <pritamkumar72002@gmail.com>
2023-03-29 20:17:32 +0530

    user created

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,10 +1,39 @@
 const { User } = require("../models");
 
 const createUser = async (req, res) => {
+ try{
+  const {
+    email,
+    name,
+    gender,
+    password,
+    phone,
+    college
+  } = req.body;
+  const newUser=new User({
+    email,
+    name,
+    gender,
+    password,
+    phone,
+    college
+  })
+  const createUser = await newUser.save();
+  res.status(200).send(createUser);
+
+ }catch(error){
+  res.status(500).send(error.message);
+ }
+
 };
 
 const getAllUser = async (req, res) => {
- 
+  try {
+    const getUsers = await User.find();
+    res.status(200).send(getUsers);
+  } catch (error) {
+    res.status(500).send({ message: "internal server error" });
+  }
 };
 
 module.exports = {
