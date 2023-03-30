c94ae60b8efa0ca4712da800ad10c4d26f468e0c
Dev Ashrit Behera <devashritcode@gmail.com>
2023-03-30 09:36:47 +0530

    login and signup complete
    Signed-off-by: Dev Ashrit Behera <devashritcode@gmail.com>

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,4 +1,6 @@
 const { User } = require("../models");
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const createUser = async (req, res) => {
  try{
@@ -10,11 +12,22 @@ const createUser = async (req, res) => {
     phone,
     college
   } = req.body;
+
+  //finding existing users
+  const oldUser = await User.find({ email })
+    if (oldUser.length != 0) {
+      return res.status(401).json({ error: 'User Already Exists' })
+    }
+
+  //password hashing
+  const salt = bcrypt.genSaltSync(10);
+  const bcrypt_password = bcrypt.hashSync(password, salt);
+
   const newUser=new User({
     email,
     name,
     gender,
-    password,
+    password : bcrypt_password,
     phone,
     college
   })
@@ -36,7 +49,33 @@ const getAllUser = async (req, res) => {
   }
 };
 
+const loginUser = async (req, res) => {
+  const { email, password } = req.body
+  const oldUser = await User.findOne({ email })
+  if (!oldUser) {
+    res.status(404).json({
+      msg: 'User Not Found',
+    })
+  }
+ 
+  if (await bcrypt.compare(password, oldUser.password)) {
+    const token = jwt.sign({email: oldUser.email, name: oldUser.name,phone: oldUser.phone}, process.env.JWT_SECRET)
+
+    if (res.status(201)) {
+      return res.status(201).send({token :  token })
+    } 
+    else {
+      return res.status(500).send({ message: 'error' })
+    }
+  }
+  res.status(401).send({message : "Invalid password"})
+}
+
+
 module.exports = {
   createUser,
   getAllUser,
+  loginUser
 };
+
+
--- src/routes/user.js
+++ src/routes/user.js
@@ -5,4 +5,5 @@ const {UserController}=require("../controllers");
 
 route.post("/create/user",UserController.createUser);
 route.get("/get/user",UserController.getAllUser);
+route.post("/login/user",UserController.loginUser);
 module.exports=route;
